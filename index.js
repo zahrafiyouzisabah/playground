0f83712fe2378c49fc4539bf85e783ab32b1e20f
Zahra Sabah <zahrasabah46@gmail.com>
2026-01-17 12:03:50 -0500

    Add tests

--- index.js
+++ index.js
@@ -3,5 +3,8 @@ function fibonacci(n) {
     return fibonacci(n - 1) + fibonacci(n - 2);
 }
 
+
 console.log(fibonacci(10)); 
 console.log(fibonacci(15));
+
+module.exports = { fibonacci };
---
+++ index.test.js
@@ -0,0 +1,13 @@
+const {fibonacci} = require('./index');
+
+test('fibonacci of 0 should be 0', () => {
+    expect(fibonacci(0)).toBe(0);
+});
+
+test('fibonacci of 1 should be 1', () => {
+    expect(fibonacci(1)).toBe(1);
+});
+
+test('fibonacci of 5 should be 5', () => {
+    expect(fibonacci(5)).toBe(5);
+})
